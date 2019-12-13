1454a30ea3876cb7833e5e40190fafc6d618342c
simonbassey <simon.dev.bassey@gmail.com>
2019-12-13 15:17:48 +0100

    Added concrete implementation for TodoService abstraction

--- src/main/java/com/simonbassey/springtodo/core/services/TodoService.java
+++ src/main/java/com/simonbassey/springtodo/core/services/TodoService.java
@@ -1,28 +1,53 @@
 package com.simonbassey.springtodo.core.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import com.simonbassey.springtodo.core.abstractions.repositories.ITodoRepository;
 import com.simonbassey.springtodo.core.abstractions.services.ITodoService;
 import com.simonbassey.springtodo.core.domain.entities.Todo;
 
 public class TodoService implements ITodoService {
 
+	private final ITodoRepository _todoRepository;
+	
+	@Autowired
+	public TodoService(ITodoRepository repository) {
+		_todoRepository = repository;
+	}
+	
 	@Override
 	public Todo CreateTodo(Todo todoEntity) {
-		// TODO Auto-generated method stub
-		return null;
+		Objects.requireNonNull(todoEntity);
+		try {
+			Todo res = this._todoRepository.addTodo(todoEntity);
+			return res;
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		}
 	}
 
 	@Override
 	public List<Todo> GetTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<Todo> todos = new ArrayList<>();
+			this._todoRepository.getTodos();
+			return todos;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 	@Override
 	public List<Todo> GetTodos(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return _todoRepository.getTodosByUserId(userId);
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 
 }
