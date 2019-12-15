9527869f951de536ad556b59b51ea2ff7698ca01
simonbassey <simon.dev.bassey@gmail.com>
2019-12-15 11:28:05 +0100

    Configured http basic security

---
+++ src/main/java/com/simonbassey/springtodo/infrastructure/config/AppSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.simonbassey.springtodo.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.simonbassey.springtodo.core.abstractions.services.IUserService;
+import com.simonbassey.springtodo.infrastructure.security.AppUserDetailService;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final AppUserDetailService _userDetailService;
+	public AppSecurityConfig(AppUserDetailService userService) {
+		this._userDetailService = userService;
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder builder) throws Exception {
+		builder.userDetailsService(_userDetailService);
+	}
+	
+	@Bean
+	protected BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable();
+		httpSecurity.
+			authorizeRequests()
+			.antMatchers("/api/accounts/create").anonymous()
+			.antMatchers("/api/**").fullyAuthenticated()
+			.and()
+			.httpBasic();
+	}
+
+}
